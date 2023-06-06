e39b81d8fa9228a304c4eef6c5ece7b0faa914d5
sweedy12 <nir.sweed@mail.huji.ac.il>
2023-06-06 14:07:29 +0300

    firebase start5

--- inter.js
+++ inter.js
@@ -1,4 +1,10 @@
 <script type="module">
-import {get_from_database} from "./firebase.js"
-window.get_from_database = get_from_database;
+import { get_from_database } from "./firebase.js";
+
+async function init() {
+  await import("./firebase.js");
+  window.get_from_database = get_from_database;
+}
+
+init();
 <\script>
