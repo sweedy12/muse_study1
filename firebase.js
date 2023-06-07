71d8af20e76cc6ce6c3bcb0368e301ce76d1c173
sweedy12 <nir.sweed@mail.huji.ac.il>
2023-06-08 02:41:44 +0300

    firebase working, with alerts

--- firebase.js
+++ firebase.js
@@ -6,15 +6,7 @@
   // https://firebase.google.com/docs/web/setup#available-libraries
 
   // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyB9YPwt1mF2m6qG-UXiEGCo_F7iDKaEhO4",
-    authDomain: "muse-c41fd.firebaseapp.com",
-    databaseURL: "https://muse-c41fd-default-rtdb.firebaseio.com",
-    projectId: "muse-c41fd",
-    storageBucket: "muse-c41fd.appspot.com",
-    messagingSenderId: "313728899350",
-    appId: "1:313728899350:web:f309d4b02c049bac4c5837"
-  };
+  
 
   // Initialize Firebase
   const firebase = initializeApp(firebaseConfig);
